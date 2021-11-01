2b46910c7d64b5a7c355f873937a7261ef4a15f9
enesbora <boraenes84@gmail.com>
2021-11-01 20:00:48 +0100

    fonksiyonlar

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,21 @@
-function addToChart(prductName) {
-    console.log("Sepete eklendi" : + prductName)
+function addToChart(quantity, prductName = "Elma") {
+    console.log("Sepete eklendi : ürün : "+ prductName + " adet : " + quantity)
 }
 
+// addToChart("Elma")
+addToChart(10)
+// addToChart("Karpuz")
+// addToChart(15)
 
+
+let sayHello = () => {
+    console.log("Hello World!")
+}
+
+sayHello()
+
+let sayHello2 = function () {
+    console.log("Hello World 2")
+}
+
+sayHello2();
