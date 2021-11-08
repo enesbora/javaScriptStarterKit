02f33e28b29326f5ba86c5b940a32d8debd34814
enesbora <boraenes84@gmail.com>
2021-11-08 22:52:15 +0100

    oop

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,18 +1,22 @@
+import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js";
 import User from "../models/user.js";
 import UserService from "../services/userService.js";
 
 console.log("User component loaded");
 
-let userService = new UserService();
+let logger1 = new MongoLogger();
+let userService = new UserService(logger1);
 
-let user1 = new User();
-userService.add();
+let user1 = new User(1, "Enes", "Bora", "Balikesir");
+let user2 = new User(2, "Ali Etka", "Bora", "Balikesir");
+
+userService.add(user1);
+userService.add(user2);
+
+console.log(userService.list());
+console.log(userService.getById(1));
 
 userService.getById(1);
-userService.list();
-
-
-
 
 let customer = { id: 1, firstName: "Engin" };
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,13 +1,19 @@
 export default class UserService {
+  constructor(loggerService) {
+    this.users = [];
+    this.loggerService = loggerService;
+  }
+
   add(user) {
-    console.log("kullanici eklendi" + user);
+    this.users.push(user);
+    this.loggerService.log(user);
   }
 
   list() {
-    console.log("kullanicilar listelendi");
+    return this.users;
   }
 
-  getById() {
-    console.log("kullanici id ile cagirildi");
+  getById(id) {
+    return this.users.find(u => u.id === id);
   }
 }
